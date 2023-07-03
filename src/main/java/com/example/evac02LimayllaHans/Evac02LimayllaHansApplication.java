3851c599a885b6b1e57c1c671366d6755ce142a1
hans <hanslc146@gmail.com>
2023-07-03 12:18:28 -0500

    cambios01

---
+++ src/main/java/com/example/evac02LimayllaHans/Evac02LimayllaHansApplication.java
@@ -0,0 +1,13 @@
+package com.example.evac02LimayllaHans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Evac02LimayllaHansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Evac02LimayllaHansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/evac02LimayllaHans/Evac02LimayllaHansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.evac02LimayllaHans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Evac02LimayllaHansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
